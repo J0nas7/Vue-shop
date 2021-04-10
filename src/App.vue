c5bfe57079435f04e9847d90775626a73c130ea5
Jonas Sørensen <jonas7793@gmail.com>
2021-04-10 22:29:08 +0200

    Admin button clickable

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="app" class="container">
-    <h1>Skillema-dinga-du shop</h1>
-    <button id="admin">Admin</button>
+    <div class="topbar">
+      <h1>Skillema-dinga-du vue-shop</h1>
+      <button @click="goToAdmin" id="admin">Admin</button>
+    </div>
     <router-view/>
     <div class="clrBth"></div>
   </div>
 </template>
+
+<script>
+import { useRouter } from 'vue-router'
+
+export default {
+  setup() {
+    const router = useRouter()
+
+    const goToAdmin = () => {
+      router.push({ name: 'AdminArea' })
+    }
+
+    return { goToAdmin }
+  }
+}
+</script>
---
+++ src/views/Admin.vue
@@ -0,0 +1,13 @@
+<template>
+  admin area
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
